218629a41e27ef1dc475551fb51a52649ad34c8c
HellPie <git@hellpie.dev>
2022-03-19 11:48:52 +0200

    Update header with constants

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 19:19:53 by drossi            #+#    #+#             */
-/*   Updated: 2022/02/11 19:33:53 by drossi           ###   ########.fr       */
+/*   Updated: 2022/03/19 11:28:40 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # ifndef BUFF_SIZE
 #  define BUFF_SIZE 8
 # endif
+
+# define GNL_FD_MAX 10496
+
+# define GNL_ERR -1
+# define GNL_OK 1
+# define GNL_EOF 0
 
 int	get_next_line(const int fd, char **line);
 
