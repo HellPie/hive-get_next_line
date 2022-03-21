1301f2d27d58dc199f7fca4da72312af235b0705
HellPie <git@hellpie.dev>
2022-03-21 20:54:38 +0200

    Fix hacky cast to suppress unused argument warnings in get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 19:20:01 by drossi            #+#    #+#             */
-/*   Updated: 2022/03/21 19:31:52 by drossi           ###   ########.fr       */
+/*   Updated: 2022/03/21 20:33:58 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	get_next_line(const int fd, char **line)
 {
-	(void *)fd;
-	(void *)line;
+	(void)fd;
+	(void)line;
 	return (GNL_ERR);
 }
