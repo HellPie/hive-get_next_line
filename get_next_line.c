d185a2346201cfd9479603e09d9eaa3cff34feff
HellPie <git@hellpie.dev>
2022-03-24 18:02:33 +0200

    Rewrite get_next_line using spans

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,73 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 19:20:01 by drossi            #+#    #+#             */
-/*   Updated: 2022/03/21 20:33:58 by drossi           ###   ########.fr       */
+/*   Updated: 2022/03/24 17:51:09 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include "ft_string.h"
+#include "ft_extra.h"
+#include "ft_span.h"
 #include "get_next_line.h"
+
+static t_bool	_read_fd(int fd, t_span *span, ssize_t *out_s_read)
+{
+	if (span->s_data - span->len < BUFF_SIZE)
+	{
+		if (!ft_span_res(span, span->s_data + BUFF_SIZE))
+		{
+			*out_s_read = -1;
+			return (FALSE);
+		}
+	}
+	*out_s_read = read(fd, span->data + span->len, BUFF_SIZE);
+	return (out_s_read > 0);
+}
+
+static int	_crop_str(char **out, t_span *span, size_t len)
+{
+	*out = ft_strsub(span->data, 0, len);
+	if (!*out)
+	{
+		ft_span_del(span);
+		return (GNL_ERR);
+	}
+	if (span->len != len)
+	{
+		span->len -= len + 1;
+		ft_memcpy(span->data, span->data + len + 1, span->len);
+	}
+	else
+	{
+		span->len = 0;
+	}
+	return (GNL_OK);
+}
 
 int	get_next_line(const int fd, char **line)
 {
-	(void)fd;
-	(void)line;
-	return (GNL_ERR);
+	static t_span	fds[GNL_FD_MAX];
+	char			*endl;
+	ssize_t			s_read;
+
+	if (fd < 0 || fd >= GNL_FD_MAX || !line || read(fd, NULL, 0))
+		return (GNL_ERR);
+	if (!fds[fd].data && ft_span_new(&fds[fd], BUFF_SIZE, sizeof(char)))
+		return (GNL_ERR);
+	s_read = 1;
+	endl = ft_memchr(fds[fd].data, '\n', fds[fd].len);
+	while (!endl && _read_fd(fd, &fds[fd], &s_read))
+	{
+		endl = ft_memchr(fds[fd].data + fds[fd].len, '\n', (size_t)s_read);
+		fds[fd].len += (size_t)s_read;
+	}
+	if (s_read < 0 || (!s_read && !fds[fd].len))
+	{
+		ft_span_del(&fds[fd]);
+		return (s_read);
+	}
+	if (endl)
+		return (_crop_str(line, &fds[fd], endl - (char *)fds[fd].data));
+	return (_crop_str(line, &fds[fd], fds[fd].len));
 }
