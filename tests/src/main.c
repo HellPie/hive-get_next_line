3b2781c6f2ccd33850f6386c0721a2cdd2426cef
HellPie <git@hellpie.dev>
2022-03-27 19:30:43 +0300

    Rewrite test code for better output

--- tests/src/main.c
+++ tests/src/main.c
@@ -6,84 +6,40 @@
 /*   By: drossi <drossi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 22:50:11 by drossi            #+#    #+#             */
-/*   Updated: 2022/03/27 19:29:34 by drossi           ###   ########.fr       */
+/*   Updated: 2022/03/27 19:30:24 by drossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include "get_next_line.h"
-
-static char	*_tos(int i)
-{
-	if (i == GNL_OK)
-		return ("[  OK  ] ");
-	if (i == GNL_EOF)
-		return ("[ DONE ] ");
-	if (i == GNL_ERR)
-		return ("[ WARN ] ");
-	return ("[!ERROR] ");
-}
-
-static int	_proc_stdio(void)
-{
-	char	*line;
-	int		result;
-
-	while (1)
-	{
-		result = get_next_line(0, &line);
-		printf("%sFD: STDIN, L: \"%s\"\n", _tos(result), line);
-		if (result != 1)
-			return (result);
-	}
-}
-
-static int	_proc_files(int *fds, int count)
-{
-	int		i_fds;
-	char	*line;
-	int		result;
-	int		completed;
-
-	completed = 0;
-	while (completed < count)
-	{
-		i_fds = -1;
-		while (++i_fds < count)
-		{
-			if (fds[i_fds] < 3 || read(fds[i_fds], NULL, 0))
-				continue ;
-			result = get_next_line(fds[i_fds], &line);
-			printf("%sFD: %2d, L: \"%s\"\n", _tos(result), fds[i_fds], line);
-			if (result == 1)
-				free(line);
-			else
-			{
-				completed++;
-				fds[i_fds] = -1;
-			}
-		}
-	}
-	return (0);
-}
 
 int	main(int argc, char **argv)
 {
-	int		fds[GNL_FD_MAX];
-	int		i_fds;
+	int		fd;
+	int		out;
+	char	*line;
 
-	if (argc < 2)
+	if (argc < 1)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	out = 1;
+	while (out == 1)
 	{
-		return (_proc_stdio());
+		out = get_next_line(fd, &line);
+		if (line)
+		{
+			printf("%s\n", line);
+			free(line);
+			line = NULL;
+		}
+		else
+			printf("---ERROR--- LINE IS NULL ---ERROR---\n");
 	}
+	if (out == 0)
+		printf("---END--- REACHED EOF ---END---\n");
 	else
-	{
-		i_fds = 0;
-		while (++i_fds < argc)
-			fds[i_fds - 1] = open(argv[i_fds], O_RDONLY);
-		return (_proc_files(fds, argc - 1));
-	}
+		printf("---END--- ERROR: %d ---END---\n", out);
+	return (0);
 }
